74e0919beeaf56cafb235c02f88f00b2c706d410
Christian Roeder <christianar@msn.com>
2021-04-03 13:35:31 -0500

    footer, installed packages

--- frontend/src/views/templates/header-anonymous.vue
+++ frontend/src/views/templates/header-anonymous.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav style="backgroundColor: #1C2331" class="navbar navbar-expand-lg navbar-dark">
     <ul class="navbar-nav">
       <li class="nav-item">
         <span class="navbar-brand">Outback AutoRepair</span>
